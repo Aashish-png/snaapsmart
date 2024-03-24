6cecd8408059c92146cc00f23525e9b9229492cf
Aashish-png <aashishsarwa512@gmail.com>
2024-03-24 14:21:20 +0530

    Update user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -21,10 +21,10 @@ const UserSchema = new Schema({
     required: true,
   },
   firstName: {
-    type: String,required: true,
+    type: String,
   },
   lastName: {
-    type: String, required: true,
+    type: String,
   },
   planId: {
     type: Number,
@@ -39,34 +39,3 @@ const UserSchema = new Schema({
 const User = models?.User || model("User", UserSchema);
 
 export default User;
-
-// import { Document, Schema, model, models } from "mongoose";
-
-// export interface User extends Document {
-//     clerkId: string;
-//     email: string;
-//     username: string;
-//     photo?: string; // Assuming photo is a URL or file path
-//     firstName: string;
-//     lastName: string;
-//     planId: string;
-//     creditBalance: number;
-//     createdAt?: Date;
-//     updatedAt?: Date;
-// }
-
-// const UserSchema = new Schema({
-//     clerkId: { type: String, required: true , unique:true},
-//     email: { type: String, required: true , unique:true },
-//     username: { type: String, required: true , unique:true},
-//     photo: { type: String ,required:true},
-//     firstName: { type: String },
-//     lastName: { type: String  },
-//     planId: { type: Number,default:1 },
-//     creditBalance: { type: Number, default: 10 },
-  
-// });
-
-// const User = models?.User || model<User>('User', UserSchema);
-
-// export default User;
